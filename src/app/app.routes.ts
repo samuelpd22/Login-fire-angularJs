1ef824896163ec809933ac9c0fc5902241699375
samuelpd22 <samuel050101@hotmail.com>
2025-02-19 20:29:45 -0300

    init project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './_component/login/login.component';
+import { RegistroComponent } from './_component/registro/registro.component';
+import { RecuperarSenhaComponent } from './_component/recuperar-senha/recuperar-senha.component';
+import { NovaSenhaComponent } from './_component/nova-senha/nova-senha.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path:"login", component:LoginComponent,
+},
+{
+    path:"cadastra", component: RegistroComponent
+},{
+    path:"recuperarSenha", component: RecuperarSenhaComponent
+},{
+    path:"novaSenha/:token",component: NovaSenhaComponent
+}
+];
